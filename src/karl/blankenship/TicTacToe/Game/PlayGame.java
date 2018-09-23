d89a69fb816baa0a401c68da1ff5861f3bb31129
Karl Blankenship <Karleb777@gmail.com>
2018-09-23 08:32:52 -0500

    completed draft of code and ran but need to fix array indexes.

--- src/karl/blankenship/TicTacToe/Game/PlayGame.java
+++ src/karl/blankenship/TicTacToe/Game/PlayGame.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Random;
-import javax.swing.JOptionPane;
 
 /**
  * This class contains the main method for playing the game.
@@ -18,7 +17,7 @@ public class PlayGame {
 
     private static List<Square> squares; // Will hold a list of Square objects.
     private static boolean gameOver; // Flag for game to determine when it is over.
-    private Random random; // For generating random numbers.
+    private static Random random; // For generating random numbers.
     private static boolean playerTurn; // Flag for knowing when it is players turn.
     private boolean emptySquares; // May turn this into a method.
     private static Scanner keyboard; // Will hold player keyboard input.
@@ -30,7 +29,7 @@ public class PlayGame {
         
         // Generate a list of square instances and set initial value.
         squares = new ArrayList();
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             squares.add(new Square(Integer.toString(i+1)));   
         }
         
@@ -46,43 +45,14 @@ public class PlayGame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Psuedo Code of how I think the game should flow.
-            /*
-                I am trying to figure out whether I should make each of the 
-                9 possible squares its own object and add them all to a
-                collection or should I create a Glid class that has fields
-                for each of the 9 squares so I can use a single instance.
-                I am going to try the Grid Class first.
-        */
                     
         // Create a new instance of PlayGame class to begin the game.
         PlayGame game = new PlayGame();
        
-
-        
-
-        // Setup a keyboard system object for keyboard player square entry.
-        Scanner keyboard = new Scanner(System.in); // 
-
-        
-        
-        
-       
-        
-        
-        
-        // Display instructions and grid
-        //grid.displayGrid();
-        //System.out.println("Please type the number of the square you"); 
-        //System.out.println("would like to select and hit <Enter>");   
-        //System.out.println("when prompted as your turn.");
-        //System.out.println("You are X's and 3 in a row wins!");
-        
-
-        
-        game.display("board");
-        game.display("welcome");
+        display("board");
+        display("welcome");
         
         
         // Set an empty squares flag (or could use a counter since 9 turns is
@@ -94,75 +64,48 @@ public class PlayGame {
             // If it is player turn
             if (playerTurn) {
                 // Prompt player to type in a square by 
-                game.display("board");
-                int sq = game.getPlayerInput(3);
-                
-                
-    
-                
-                
-   
-                // If input is valid.
-                if (input.equals("1") ||
-                    input.equals("2") ||
-                    input.equals("3") ||
-                    input.equals("4") ||
-                    input.equals("5") ||
-                    input.equals("6") ||
-                    input.equals("7") ||
-                    input.equals("8") ||
-                    input.equals("9")) {
-                    
-                    // If square is empty
-                    if
-
-                        // Update square object
-
-                        // Display updated board.
-
-                        // Change player flag so it is computers turn.
-
-                        // Check/set empty squares flag
-
-                        // Check to see if player won. 
-                            // NEED A CHECK FOR WIN METHOD.
-
-                        // If player wins, display message and 
-                        //set game flag to over
-
-                    // Else, square is already taken, Display message that 
-                    // square is already taken.
+                display("board");
+                int sq = getPlayerInput(3);
+                // Apply response to the board.
+                if (sq == -1) {
+                    gameOver = true;
+                    System.out.println("Sorry, You Lost.");
                 }
-                // Else, if input is not valid
-                    //Display a message that input is not valid
-            }       
-                
-            // ELSE (if it is computer turn)
-                // Display a message that computer is thinking.
-                
-                // Slight delay for effect.
-                
-                // Have computer randomly select an available square.
-                // Eventually add a strategy for the computer to follow.
-                // for now write a computer select method that can be updated.
-                
-                // Update the selected square instance.
-                
-                // Display updated board.
-                
-                // Change the player flag so it will become the players turn.
-                
-                // Check/set empty squares flag.
-                
-                // Check to see if Computer won using a win method.
-                
-                // If computer wins, display a message and set game flag to over
-        }       
-            
-
-        
-    }
+                else
+                    squares.get(sq).setContents("X");
+                // Check to see if player won.
+                if (checkForWin("X")) {
+                    display("board");
+                    System.out.println("Congrats! You win!");
+                }
+                else {
+                    playerTurn = false;
+                    display("board");
+                }
     
+            }    
+            else {
+                display("Board");
+                System.out.println("Computer is thinking...");
+                Thread.sleep(2000);
+                int csq = getComputerInput();
+                squares.get(csq).setContents("O");
+                if (checkForWin("O")) {
+                    display("board");
+                    System.out.println("The computer has won.");
+                }
+                else {
+                    playerTurn = true;
+                    display("board");
+                }
+            }
+                
+        }            
+         
+    }
+        
+    
+                    
     
     private static void display(String selection) {
         
@@ -171,21 +114,21 @@ public class PlayGame {
 
             System.out.println("   |   |   ");
 
-            System.out.println(" " + squares.get(0).getContents() + " | " + 
-                    squares.get(1).getContents() + " | " + 
-                    squares.get(2).getContents() + " ");
+            System.out.println(" " + squares.get(1).getContents() + " | " + 
+                    squares.get(2).getContents() + " | " + 
+                    squares.get(3).getContents() + " ");
 
             System.out.println("---+---+---");
 
-            System.out.println(" " + squares.get(3).getContents() + " | " + 
-                    squares.get(4).getContents() + " | " + 
-                    squares.get(5).getContents() + " ");
+            System.out.println(" " + squares.get(4).getContents() + " | " + 
+                    squares.get(5).getContents() + " | " + 
+                    squares.get(6).getContents() + " ");
 
             System.out.println("---+---+---");
 
-            System.out.println(" " + squares.get(6).getContents() + " | " + 
-                    squares.get(7).getContents() + " | " + 
-                    squares.get(8).getContents() + " ");
+            System.out.println(" " + squares.get(7).getContents() + " | " + 
+                    squares.get(8).getContents() + " | " + 
+                    squares.get(9).getContents() + " ");
 
             System.out.println("   |   |   ");    
         }
@@ -208,7 +151,7 @@ public class PlayGame {
     
     private static boolean emptySquares() {
       
-        for (int i = 0; i < 9; i++) {
+        for (int i = 1; i < 10; i++) {
             if (squares.get(i).squareAvailable())
                 return true;
         }
@@ -219,6 +162,7 @@ public class PlayGame {
     private static int getPlayerInput(int x) {
         keyboard = new Scanner(System.in);
                 
+        // Ask three times for a valid input and then game is over.
         while (x > 0) {
         
             display("player");
@@ -235,6 +179,47 @@ public class PlayGame {
             
         }
         return -1;  // to kill game
+    }
+    
+    private static int getComputerInput() {
+        boolean found = false;
+        int selection = 0;
+        while (!found) {
+            selection = random.nextInt(9) + 1;
+            if (squares.get(selection).squareAvailable())
+                found = true;   
+        }
+        return selection; 
+    }
+    
+    private static boolean checkForWin(String input) {
+        if ((squares.get(1).getContents().equals(input) &&
+                squares.get(2).getContents().equals(input) &&
+                    squares.get(3).getContents().equals(input)) ||
+            (squares.get(4).getContents().equals(input) &&
+                squares.get(5).getContents().equals(input) &&
+                    squares.get(6).getContents().equals(input)) ||  
+            (squares.get(7).getContents().equals(input) &&
+                squares.get(8).getContents().equals(input) &&
+                    squares.get(9).getContents().equals(input)) ||    
+            (squares.get(1).getContents().equals(input) &&
+                squares.get(4).getContents().equals(input) &&
+                    squares.get(7).getContents().equals(input)) ||    
+            (squares.get(2).getContents().equals(input) &&
+                squares.get(5).getContents().equals(input) &&
+                    squares.get(8).getContents().equals(input)) ||
+            (squares.get(3).getContents().equals(input) &&
+                squares.get(6).getContents().equals(input) &&
+                    squares.get(9).getContents().equals(input)) ||
+            (squares.get(1).getContents().equals(input) &&
+                squares.get(5).getContents().equals(input) &&
+                    squares.get(9).getContents().equals(input)) ||
+            (squares.get(7).getContents().equals(input) &&
+                squares.get(5).getContents().equals(input) &&
+                    squares.get(3).getContents().equals(input)))
+            return true;
+        else
+            return false;
     }
     
 }
